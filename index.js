0f885854b2354b2fb820af881522d0e888423aed
Deepak <deepchaurasiya1997@gmail.com>
2022-06-16 05:28:19 +0530

    performance improvements

--- index.js
+++ index.js
@@ -1,40 +1,48 @@
-console.log("This is my index js file");
+
 // 9697029f981c4f7d9c57debcf9ce8920
-let newsAccordion = document.getElementById("newsAccordion");
+console.log("This is my index js file");
+
+// Initialize the news api parameters
+let source = 'the-times-of-india';
+let apiKey = '9697029f981c4f7d9c57debcf9ce8920'
+
+// Grab the news container
+let newsAccordion = document.getElementById('newsAccordion');
+
+// Create an ajax get request
 const xhr = new XMLHttpRequest();
-let news = `<div class="card">
-<div class="card-header" id="headingOne">
-  <h2 class="mb-0">
-    <button
-      class="btn btn-link"
-      type="button"
-      data-toggle="collapse"
-      data-target="#collapseOne"
-      aria-expanded="true"
-      aria-controls="collapseOne"
-    >
-      Collapsible Group Item #1
-    </button>
-  </h2>
-</div>
+xhr.open('GET', `https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=${apiKey}`, true);
 
-<div
-  id="collapseOne"
-  class="collapse show"
-  aria-labelledby="headingOne"
-  data-parent="#accordionExample"
->
-  <div class="card-body">
-    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
-    richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard
-    dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf
-    moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
-    assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore
-    wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur
-    butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim
-    aesthetic synth nesciunt you probably haven't heard of them accusamus
-    labore sustainable VHS.
-  </div>
-</div>
-</div>`
+// What to do when response is ready
+xhr.onload = function () {
+    if (this.status === 200) {
+        let json = JSON.parse(this.responseText);
+        let articles = json.articles;
+        console.log(articles);
+        let newsHtml = "";
+        articles.forEach(function(element, index) {
+            // console.log(element, index)
+            let news = `<div class="card">
+                            <div class="card-header" id="heading${index}">
+                                <h2 class="mb-0">
+                                <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse${index}"
+                                    aria-expanded="false" aria-controls="collapse${index}">
+                                   <b>Breaking News ${index+1}:</b> ${element["title"]}
+                                </button>
+                                </h2>
+                            </div>
 
+                            <div id="collapse${index}" class="collapse" aria-labelledby="heading${index}" data-parent="#newsAccordion">
+                                <div class="card-body"> ${element["content"]}. <a href="${element['url']}" target="_blank" >Read more here</a>  </div>
+                            </div>
+                        </div>`;
+            newsHtml += news;
+        });
+        newsAccordion.innerHTML = newsHtml;
+    }
+    else {
+        console.log("Some error occured")
+    }
+}
+
+xhr.send()
